74693bbd0a430ccb4a95e4370e8972d2c713055a
Kian James F. Querubin <kianjames.querubin.f@bulsu.edu.ph>
2023-04-11 15:41:51 +0800

    step 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,11 +5,13 @@ import "rxjs/add/operator/map";
 import { Observable } from "rxjs/Observable";
 
 import { Album } from "./album";
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
   // ** Variables
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
 
   // ** Functions
 
@@ -17,6 +19,11 @@ export class ProductService {
     return this._http
       .get(this._albumUrl)
       .map((response) => <Album>response.json());
+  }
+  getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
   }
 
   constructor(private _http: Http) {}
