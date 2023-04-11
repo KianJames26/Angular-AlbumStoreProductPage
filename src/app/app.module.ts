b5be587af6fd5aba0b8b4fe90e71c0127c628af4
Kian James F. Querubin <kianjames.querubin.f@bulsu.edu.ph>
2023-04-11 16:16:32 +0800

    step 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,9 @@ import { ProductListComponent } from "./product-list/product-list.component";
 import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
-  { path: "products/", component: ProductListComponent },
-  { path: "product/id", component: ProductPageComponent },
-  { path: "", pathMatch: "full", redirectTo: "products/" },
+  { path: "products", component: ProductListComponent },
+  { path: "product/:id", component: ProductPageComponent },
+  { path: "", pathMatch: "full", redirectTo: "products" },
 ];
 @NgModule({
   declarations: [
